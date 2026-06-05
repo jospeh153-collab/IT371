95cbee7aec5d288a04bbc7c35fbc29a2f70255ee
jospeh153-collab <jospeh153@gmail.com>
2026-06-05 18:43:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function toggleInfo(id) {
     var infoBox = document.getElementById(id);
 
-    if (infoBox.style.display === "none") {
+    if (infoBox.style.display === "none" || infoBox.style.display === "") {
         infoBox.style.display = "block";
     } else {
         infoBox.style.display = "none";
